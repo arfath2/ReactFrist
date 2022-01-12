6e667e34680bdde1a6d673ccf6913d400d243d35
arfath2 <arfath23mohammed@gmail.com>
2022-01-12 18:44:22 +0530

    frist commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem() {
+    return (
+        <div><div>Food Rs 10</div>
+        <div>petrol Rs 100</div>
+        <div>Movies Rs 200</div></div>
+        
+
+    );
+}
+
+export default ExpenseItem;
