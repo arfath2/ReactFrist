afc71d0741949957c972edc589344aeb1009005f
arfath2 <arfath23mohammed@gmail.com>
2022-01-13 10:53:35 +0530

    props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,18 +1,13 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date();
-    const expenseTitle = 'car insuranse';
-    const expensePrize = 294.65;
-    const LocationOfExpenditure = 'Games';
-
+function ExpenseItem(props) {
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+            <div>{props.date.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <h2>{LocationOfExpenditure}</h2>
-                <div className='expense-item__price'>${expensePrize}</div>
+                <h2>{props.title}</h2>
+                <h2>{props.loe}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
