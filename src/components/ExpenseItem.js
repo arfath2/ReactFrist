7b3cc55ef43baa8744a313ce3354b5c30cc47e8f
arfath2 <arfath23mohammed@gmail.com>
2022-01-13 08:53:52 +0530

    LocationOfExpenditure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,12 +4,14 @@ function ExpenseItem() {
     const expenseDate = new Date();
     const expenseTitle = 'car insuranse';
     const expensePrize = 294.65;
+    const LocationOfExpenditure = 'Games';
 
     return (
         <div className='expense-item'>
             <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
                 <h2>{expenseTitle}</h2>
+                <h2>{LocationOfExpenditure}</h2>
                 <div className='expense-item__price'>${expensePrize}</div>
             </div>
         </div>
