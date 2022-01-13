e95a061bfdb5e66fc11383165ce81ca41a878fbd
arfath2 <arfath23mohammed@gmail.com>
2022-01-13 08:49:04 +0530

    raw data

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
 import './ExpenseItem.css'
 
 function ExpenseItem() {
+    const expenseDate = new Date();
+    const expenseTitle = 'car insuranse';
+    const expensePrize = 294.65;
+
     return (
         <div className='expense-item'>
-            <div>Date</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className='expense-item__description'>
-                <h2>car Insuranse</h2>
-                <div className='expense-item__price'>$294.64</div>
+                <h2>{expenseTitle}</h2>
+                <div className='expense-item__price'>${expensePrize}</div>
             </div>
         </div>
     );
