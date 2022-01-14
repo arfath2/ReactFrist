064437e6add3a5d80dbb7af4cdeb827f8cfa4b1b
arfath2 <arfath23mohammed@gmail.com>
2022-01-14 14:59:24 +0530

    FileStructure

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,18 @@
-import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import ExpenseDetails from './ExpenseDetails';
+import Card from './Card';
+import './ExpenseItem.css';
 
 function ExpenseItem(props) {
     return (
-        <div className='expense-item'>
+        <Card className='expense-item'>
             <ExpenseDate date={props.date}/>
-            <ExpenseDetails title={props.title}
-            amount={props.amount}/>
-            <h2>{props.loe}</h2>
-        </div>
-    );
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <h2>{props.loe}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
+            </div>
+        </Card>
+    ); 
 }
 
 export default ExpenseItem;
