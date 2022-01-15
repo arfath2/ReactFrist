3693aef397f3ce5c4bf902032bf672dbee4282db
arfath2 <arfath23mohammed@gmail.com>
2022-01-15 12:14:19 +0530

    frazing

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,4 +1,4 @@
-import Card from './Card';
+import Card from '../UI/Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
@@ -8,21 +8,25 @@ function Expenses(props) {
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
+        loe={props.items[0].loe}
         date={props.items[0].date}
       />
       <ExpenseItem
         title={props.items[1].title}
         amount={props.items[1].amount}
+        loe={props.items[1].loe}
         date={props.items[1].date}
       />
       <ExpenseItem
         title={props.items[2].title}
         amount={props.items[2].amount}
+        loe={props.items[2].loe}
         date={props.items[2].date}
       />
       <ExpenseItem
         title={props.items[3].title}
         amount={props.items[3].amount}
+        loe={props.items[3].loe}
         date={props.items[3].date}
       />
     </Card>
