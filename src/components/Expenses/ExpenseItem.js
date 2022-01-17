07319c84043b29fd74a76f2dee96742e3f455526
arfath2 <arfath23mohammed@gmail.com>
2022-01-17 08:28:04 +0530

    ClickEvents

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,14 +3,29 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const changeTitle = () => {
+        console.log('hello')
+    }
+
+    const deleteItem = (e)=>{
+        // if(e.target.classList.contains('delete')){
+        //     if(alert('Are You Sure?')){
+        //       var li = e.target.parentElement;
+        //       items.removeChild(Card);
+        //     }
+        //   }
+    }
+
     return (
-        <Card className='expense-item'>
+        <Card id="items" className='expense-item delete'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
                 <h2>{props.loe}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
+            <button onClick={changeTitle}>change title</button>
+            <button onClick={deleteItem}>Delete</button>
         </Card>
     ); 
 }
