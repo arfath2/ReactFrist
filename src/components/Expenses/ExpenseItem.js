13066533af179eb692a4de029ffd1fd7046f3808
arfath2 <arfath23mohammed@gmail.com>
2022-01-17 18:35:24 +0530

    useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,31 +1,33 @@
+import React,{ useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+    const [title,setTitle] = useState(props.title)
     const changeTitle = () => {
-        console.log('hello')
+        setTitle('changed')
     }
 
-    const deleteItem = (e)=>{
+    //const deleteItem = (e)=>{
         // if(e.target.classList.contains('delete')){
         //     if(alert('Are You Sure?')){
         //       var li = e.target.parentElement;
         //       items.removeChild(Card);
         //     }
         //   }
-    }
+   // }
 
     return (
         <Card id="items" className='expense-item delete'>
             <ExpenseDate date={props.date}/>
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <h2>{props.loe}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>
             <button onClick={changeTitle}>change title</button>
-            <button onClick={deleteItem}>Delete</button>
+            {/* <button onClick={deleteItem}>Delete</button> */}
         </Card>
     ); 
 }
